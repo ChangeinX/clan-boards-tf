e7677b9cf1773447aa7126cb4fd843b5635a9131
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-12 10:45:25 -0500

    Fix DATABASE_URL to use postgresql protocol

--- modules/static_instance/main.tf
+++ modules/static_instance/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "this" {
               amazon-linux-extras install docker -y
               service docker start
               docker run -d --restart=always \
-                -e DATABASE_URL=postgres://postgres:${var.db_password}@${var.db_endpoint}:5432/postgres \
+                -e DATABASE_URL=postgresql://postgres:${var.db_password}@${var.db_endpoint}:5432/postgres \
                 ${var.image}
               EOT
 }
