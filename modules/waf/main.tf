e57b23a7bb433d744f4765f8ab9d62b51aaf50c6
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-28 15:46:12 -0500

    Fix WAF association

---
+++ modules/waf/main.tf
@@ -0,0 +1,36 @@
+resource "aws_wafv2_web_acl" "this" {
+  name  = "${var.app_name}-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.app_name}-waf"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+}
